88091542ed6bfb7274842825ecfdb5823188011a
GamerninjaSKYT <gamerninjacz@gmail.com>
2024-07-28 08:33:22 +0200

    Crafting ui complete

--- CraftingUI.gd
+++ CraftingUI.gd
@@ -8,9 +8,20 @@ extends ScrollContainer
 func _ready():
 	LoadRecipes()
 func LoadRecipes():
+	for c in recipes_ui.get_children():
+		c.queue_free()
 	var m = get_tree().root.get_child(0).craftman
 	for r in m.recipes:
 		if r.ingredients.size() <= ingredientslots.size():
-			print(r.result.item.name)
 			var p = recipepackedscene.instantiate()
 			recipes_ui.add_child(p)
+			for im in p.ingredientimages:
+				im.visible = false
+			var n = 0
+			for i in r.ingredients:
+				p.ingredientimages[n].visible = true
+				p.ingredientimages[n].texture = i.item.image
+				p.ingredientcounts[n].text = str(i.count)
+				n += 1
+			p.resultimage = r.result.item.image
+			p.resultcount.text = str(r.result.count)
---
+++ recipeui.gd
@@ -0,0 +1,7 @@
+class_name recipeui
+extends Control
+
+@export var ingredientimages:Array[TextureRect]
+@export var ingredientcounts:Array[Label]
+@export var resultimage:TextureRect
+@export var resultcount:Label
